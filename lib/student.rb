4c83d48127dae48037c61cb70e67974403ea92c1
Harry Wilson <harry.wilson1995a@gmail.com>
2020-02-25 22:24:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,13 @@ end
     DB[:conn].execute(sql)
   end
   
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end 
+  
   def self.new_from_db(row)
     new_student = self.new 
     new_student.id = row[0]
