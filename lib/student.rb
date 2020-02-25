12ffed4623002e8c1b4535bb7e20aa85064f03cf
Harry Wilson <harry.wilson1995a@gmail.com>
2020-02-25 22:28:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,11 +51,9 @@ end
     student 
   end 
   
-  def self.new_from_db(row)
-    new_student = self.new 
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+  def self.new_from_db(array)
+    new_student = self.new(array[0], array[1], array[2])
+
     new_student
   end
   
